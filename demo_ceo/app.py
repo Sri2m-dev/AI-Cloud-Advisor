9d382c571b06c383ffe39d409d383aaa633752f7
srikanth2m-dev <srikanth2.m@gmail.com>
2026-03-31 07:16:04 +0530

    demo

--- demo_ceo/app.py
+++ demo_ceo/app.py
@@ -1,10 +1,6 @@
 
-st.write("🚀 Before dashboard call")
 import streamlit as st
-import os
-
-ENV = os.getenv("APP_ENV", "demo")
-st.write("ENV VALUE:", ENV)
 
 def main():
-    st.title("🚀 Demo CEO Dashboard")
+    st.write("🚀 Before dashboard call")
+    st.title("Demo Dashboard")
